fc0066b279afc241082cfafceee20b95c18e27b3
Peter Anderson <pete.anderson@gmail.com>
2019-05-13 21:16:47 +0200

    Got basic lines being added on click and player turn changing

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 console.log("sanity check");
+
+var board = document.getElementsByClassName('board')[0];
+var rows = '';
+var html = '';
+var boardSize = 6;
+var playerTurn = 1;
+
+for (var i = 0; i < boardSize; i++) {
+    rows += '<div class="row row' + i + '"></div>';
+}
+
+for (var j = 0; j < boardSize; j++) {
+    html += '<div class="col col'+ j + '">'+ rows + '</div>';
+}
+
+board.innerHTML = html;
+
+board.addEventListener('click', function(e){
+    e.stopPropagation();
+    // console.log(e.target.classList);
+    // console.log(e);
+    if (e.offsetY < 10) {
+        if (e.target.classList.contains('border-top')) {
+            return;
+        }
+        playerTurn === 1 ? e.target.classList.add('border-top') : e.target.classList.add('border-top-player2');
+        playerTurn === 1 ? playerTurn = 2 : playerTurn = 1;
+    } else if (e.offsetX < 10) {
+        if (e.target.classList.contains('border-left')) {
+            return;
+        }
+        playerTurn === 1 ? e.target.classList.add('border-left') : e.target.classList.add('border-left-player2');
+        playerTurn === 1 ? playerTurn = 2 : playerTurn = 1;
+    }
+});
